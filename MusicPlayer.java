8e490cd590f4668cc1feeef6191a6205b2cbead1
famalone <famalone@NN123-HA-17.HS.sd203.org>
2015-04-23 11:26:26 -0500

    Finished project

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -6,44 +6,49 @@ import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 /**
- * 
+ * Fin Malone
+ * 04/23/2015
+ * Implements a GUI 
  */
 public class MusicPlayer
 {
     private static final int FRAME_WIDTH = 500;
-    private static final int FRAME_HEIGHT = 500;
+    private static final int FRAME_HEIGHT = 100;
 
     private JFrame frame;
     private JPanel panel;
-    private JButton guitar, bass, strings, melody;
+    private JButton guitar, bass, strings, melody, play;
     private JLabel label;
-    
+
     private MusicCreator create;
     private Pattern intro, verse, jukebox;
-    
+
     public MusicPlayer()
     {
         jukebox = new Pattern("KEmin T[80]");
         intro = new Pattern("KEmin T[80]");
         verse = new Pattern("KEmin T[80]");
         create = new MusicCreator();
-        
-        
+
         frame = new JFrame();
         panel = new JPanel();
+        
         guitar = new JButton("guitar");
         panel.add(guitar);
 
         bass = new JButton("bass");
         panel.add(bass);
-        
+
         strings = new JButton("strings");
         panel.add(strings);
-        
+
         melody = new JButton("melody");
         panel.add(melody);
-        
-        label = new JLabel("No song to play");
+
+        play = new JButton("play");
+        panel.add(play);
+
+        label = new JLabel("ENJOY!!!!!");
         panel.add(label);
 
         frame.add(panel);
@@ -53,13 +58,14 @@ public class MusicPlayer
         bass.addActionListener(listener);
         strings.addActionListener(listener);
         melody.addActionListener(listener);
+        play.addActionListener(listener);
 
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
     }
-    
+
     public class ClickListener implements ActionListener
     {
         public void actionPerformed(ActionEvent event)
@@ -68,38 +74,33 @@ public class MusicPlayer
             {
                 verse.add(create.guitar());
             }
-            else if(event.getActionCommand().equals("bass"))
+            if(event.getActionCommand().equals("bass"))
             {
                 intro.add(create.synthBass(),4);
                 verse.add(create.synthBass(),14);
             }
-            else if(event.getActionCommand().equals("strings"))
+            if(event.getActionCommand().equals("strings"))
             {
                 intro.add(create.synthStrings(),2);
                 verse.add(create.synthStrings(),7);
             }
-            else if(event.getActionCommand().equals("melody"))
+            if(event.getActionCommand().equals("melody"))
             {
                 verse.add(create.synthMelody());
             }
-            else
+            if(event.getActionCommand().equals("play"))
             {
-                label.setText("Must add instruments");
+                jukebox.add(intro);
+                jukebox.add(verse);
+                
+                Player player = new Player();
+                player.play(jukebox);
             }
         }
     }
-    
+
     public static void main(String[] args)
     {
-        Pattern jukebox = new Pattern("KEmin T[80]");
-        Pattern intro = new Pattern("KEmin T[80]");
-        Pattern verse = new Pattern("KEmin T[80]");
-        MusicCreator create = new MusicCreator();
-
-        jukebox.add(intro);
-        jukebox.add(verse);
-
-        Player player = new Player();
-        player.play(jukebox);
+        MusicPlayer viewer = new MusicPlayer();
     }
 }
